6e9a0897c70fdb3c1411d16f45871a142380aabc
133332D <loh_huiru@hotmail.com>
2015-09-18 11:13:36 +0800

    webservice insert update delete

--- Facility Reservation Kiosk/ReadFacilityService/Program.cs
+++ Facility Reservation Kiosk/ReadFacilityService/Program.cs
@@ -33,6 +33,27 @@ namespace ReadFacilityService
             this.desc = d;
         }
     }
+
+    class SqlObject
+    {
+        public string departmentID { get; set; }
+
+        public SqlObject(string id)
+        {
+            this.departmentID = id;
+        }
+    }
+
+    class SqlFacObject
+    {
+        public string facilityID { get; set; }
+
+        public SqlFacObject(string id)
+        {
+            this.facilityID = id;
+        }
+    }
+
     class FacilityObject
     {
         public string faccode { get; set; }
@@ -76,7 +97,7 @@ namespace ReadFacilityService
                 doc.AppendChild(docNode);
 
                 //load xml to xml doc object
-                string xml = HttpUtility.UrlDecode(ws.getFRSLocationXML(args[1]));
+                string xml = HttpUtility.UrlDecode(ws.getFRSLocationXML("STAFF"));
                 doc.LoadXml(xml);
 
                 //set settings to indent the output
@@ -89,12 +110,14 @@ namespace ReadFacilityService
                 doc.Save(@"C:\test\GetFRSLocation.xml");
                 //Console.WriteLine();
 
-
                 //**Extract the values from XML
                 // Declare the xpath for finding objects inside the XML file
                 XmlNodeList XmlDocNodes = doc.SelectNodes("/facility/faclocation");
 
                 // Define a new List, to store the objects we pull out of the XML
+                //create a list of DepartmentID (Identifier) MSSQL database eg. SIT
+                //A
+                List<string> DepartmentListString = new List<string>();
                 List<DepartmentObject> DepartmentList = new List<DepartmentObject>();
 
                 // Loop through the nodes, extracting Facility information.
@@ -104,6 +127,30 @@ namespace ReadFacilityService
                     DepartmentObject obj = new DepartmentObject(node["code"].InnerText,
                                                         node["desc"].InnerText);
                     DepartmentList.Add(obj);
+                    DepartmentListString.Add(node["code"].InnerText);
+                }
+
+                //checks database and synchronise data by comparing
+                //the rows
+                //Insert, update and delete accordingly
+                //B
+                //create a list of string of department object
+                List<string> sqlDepartmentListString = new List<string>(); 
+                //create a list of object (SQL database)
+                List<SqlObject> sqlDepartmentList = new List<SqlObject>();
+
+                //select from the database to insert to sqlDepartmentList
+                using (var db = new KioskContext())
+                {
+                    var listofdep = from d in db.Departments
+                                    select new { d.DepartmentID };
+
+                    foreach (var department in listofdep)
+                    {
+                        SqlObject sqlObject = new SqlObject(department.DepartmentID);
+                        sqlDepartmentList.Add(sqlObject);
+                        sqlDepartmentListString.Add(department.DepartmentID);
+                    }
                 }
 
                 // Loop through the list, and print all the FacilityObjects to screen
@@ -115,18 +162,50 @@ namespace ReadFacilityService
                     Console.WriteLine(DepartmentList[i].desc);
                     Console.Write("\n");
 
-                    //insert the facility codes into the database dbo.department (DepartmentID)
-                    using (var db = new KioskContext())
+
+                    if (sqlDepartmentListString.Contains(DepartmentList[i].code) == false)
                     {
-                        var departmentID = DepartmentList[i].code;
-                        var description = DepartmentList[i].desc;
+                        //insert the facility codes into the database dbo.department (DepartmentID)
+                        using (var db = new KioskContext())
+                        {
+                            var departmentID = DepartmentList[i].code;
+                            var description = DepartmentList[i].desc;
 
-                        var department = new Department { DepartmentID = departmentID, Description = description};
-                        db.Departments.Add(department);
-                        db.SaveChanges();
+                            var department = new Department { DepartmentID = departmentID, Description = description };
+                            db.Departments.Add(department);
+                            db.SaveChanges();
+                        }
                     }
+                    else
+                    {
+                        //update the record
+                        using (var db = new KioskContext())
+                        {
+                            Department department = db.Departments.Find(DepartmentList[i].code);
 
+                            department.DepartmentID = DepartmentList[i].code;
+                            department.Description = DepartmentList[i].desc;
+                            db.SaveChanges();
+                        }
+                    }
                 }
+
+                //loop throught the sql list and find extra records to delete
+                int ListSizeSQL = sqlDepartmentList.Count;
+                for (int i = 0; i < ListSizeSQL; i++)
+                {
+                    if(DepartmentListString.Contains(sqlDepartmentList[i].departmentID) == false)
+                    {
+                        //delete the extra record found
+                        using (var db = new KioskContext())
+                        {
+                            Department department = db.Departments.Find(sqlDepartmentList[i].departmentID);
+
+                            db.Departments.Remove(department);
+                            db.SaveChanges();
+                        }
+                    }
+                }         
             }
             if (args[0] == "getFRSListXML")
             {
@@ -145,7 +224,7 @@ namespace ReadFacilityService
                 //load xml to xml doc object
                 //args[4] is the date in format DD-MMM-YYYY 09-SEP-2015
                 string xml = HttpUtility.UrlDecode(ws.getFRSListXML(
-                    "STAFF", school, "", args[4], "", "", "", "", "", "Y"));
+                    "STAFF", school, "", "09-SEP-2015", "", "", "", "", "", "Y"));
                 doc.LoadXml(xml);
 
 
@@ -165,6 +244,7 @@ namespace ReadFacilityService
 
                 // Define a new List, to store the objects we pull out of the XML
                 List<FacilityObject> FacilityList = new List<FacilityObject>();
+                List<string> FacilityListString = new List<string>();
 
                 // Loop through the nodes, extracting Facility information.
                 // We can then define a facility object and add it to the list.
@@ -183,8 +263,33 @@ namespace ReadFacilityService
                             node["openhrs"].InnerText, node["closehrs"].InnerText, node["maxbktme"].InnerText, node["maxbkunits"].InnerText,
                             node["minbktme"].InnerText, node["minbkunits"].InnerText);
                         FacilityList.Add(obj);
+
+                        FacilityListString.Add(node["faccode"].InnerText);
                     }
 
+                }
+
+                //checks database and synchronise data by comparing
+                //the rows
+                //Insert, update and delete accordingly
+                //B
+                //create a list of string of department object
+                List<string> sqlFacilityListString = new List<string>();
+                //create a list of object (SQL database)
+                List<SqlFacObject> sqlFacilityList = new List<SqlFacObject>();
+
+                //select from the database to insert to sqlDepartmentList
+                using (var db = new KioskContext())
+                {
+                    var listoffac = from f in db.Facilitys
+                                    select new { f.FacilityID };
+
+                    foreach (var facility in listoffac)
+                    {
+                        SqlFacObject sqlObject = new SqlFacObject(facility.FacilityID);
+                        sqlFacilityList.Add(sqlObject);
+                        sqlFacilityListString.Add(facility.FacilityID);
+                    }
                 }
 
                 // Loop through the list, and print all the FacilityObjects to screen
@@ -218,30 +323,82 @@ namespace ReadFacilityService
                         levelnum = facname.Substring(2, 1);
                     }
 
-                    //insert the facility codes into the database dbo.department (DepartmentID)
-                    using (var db = new KioskContext())
+                    if (sqlFacilityListString.Contains(FacilityList[i].faccode) == false)
                     {
-                        var facilityID = FacilityList[i].faccode;
-                        var departmentID = school;
-                        var description = FacilityList[i].facdesc;
-                        var block = blockname;
-                        var level = levelnum;
-                        var name = FacilityList[i].faccode;
-                        var openhrs = FacilityList[i].openhrs;
-                        var closehrs = FacilityList[i].closehrs;
-                        var maxbktime = FacilityList[i].maxbktime;
-                        var maxbkunits = FacilityList[i].maxbkunits;
-                        var minbktime = FacilityList[i].minbktime;
-                        var minbkunits = FacilityList[i].minbkunits;
+                        //insert the facility codes into the database dbo.department (DepartmentID)
+                        using (var db = new KioskContext())
+                        {
+                            var facilityID = FacilityList[i].faccode;
+                            var departmentID = school;
+                            var description = FacilityList[i].facdesc;
+                            var block = blockname;
+                            var level = levelnum;
+                            var name = FacilityList[i].faccode;
+                            var openhrs = FacilityList[i].openhrs;
+                            var closehrs = FacilityList[i].closehrs;
+                            var maxbktime = FacilityList[i].maxbktime;
+                            var maxbkunits = FacilityList[i].maxbkunits;
+                            var minbktime = FacilityList[i].minbktime;
+                            var minbkunits = FacilityList[i].minbkunits;
 
-                        var facility = new Facility { FacilityID = facilityID, DepartmentID = departmentID, Description = description, 
-                            OpenHours = openhrs, CloseHours = closehrs, MaxBkTime = maxbktime, MaxBkUnits = maxbkunits,
-                        MinBkTime = minbktime, MinBkUnits = minbkunits, Name = name, Block = block, Level = level};
-                        db.Facilitys.Add(facility);
-                        db.SaveChanges();
+                            var facility = new Facility
+                            {
+                                FacilityID = facilityID,
+                                DepartmentID = departmentID,
+                                Description = description,
+                                OpenHours = openhrs,
+                                CloseHours = closehrs,
+                                MaxBkTime = maxbktime,
+                                MaxBkUnits = maxbkunits,
+                                MinBkTime = minbktime,
+                                MinBkUnits = minbkunits,
+                                Name = name,
+                                Block = block,
+                                Level = level
+                            };
+                            db.Facilitys.Add(facility);
+                            db.SaveChanges();
+                        }
                     }
+                    else
+                    {
+                        //update the record
+                        using (var db = new KioskContext())
+                        {
+                            Facility facility = db.Facilitys.Find(FacilityList[i].faccode);
 
+                            facility.FacilityID = FacilityList[i].faccode;
+                            facility.DepartmentID = school;
+                            facility.Description = FacilityList[i].facdesc;
+                            facility.Block = blockname;
+                            facility.Level = levelnum;
+                            facility.Name = FacilityList[i].faccode;
+                            facility.OpenHours = FacilityList[i].openhrs;
+                            facility.CloseHours = FacilityList[i].closehrs;
+                            facility.MaxBkTime = FacilityList[i].maxbktime;
+                            facility.MaxBkUnits = FacilityList[i].maxbkunits;
+                            facility.MinBkTime = FacilityList[i].minbktime;
+                            facility.MinBkUnits = FacilityList[i].minbkunits;
+                            db.SaveChanges();
+                        }
+                    }
                 }
+                //loop throught the sql list and find extra records to delete
+                int ListSizeSQL = sqlFacilityList.Count;
+                for (int i = 0; i < ListSizeSQL; i++)
+                {
+                    if (FacilityListString.Contains(sqlFacilityList[i].facilityID) == false)
+                    {
+                        //delete the extra record found
+                        using (var db = new KioskContext())
+                        {
+                            Facility facility = db.Facilitys.Find(sqlFacilityList[i].facilityID);
+
+                            db.Facilitys.Remove(facility);
+                            db.SaveChanges();
+                        }
+                    }
+                }  
             }
         }
     }
