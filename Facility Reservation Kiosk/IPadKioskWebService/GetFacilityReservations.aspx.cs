2c463b54e6d7cf69b11db20c16d23e649f2c0654
133332D <loh_huiru@hotmail.com>
2015-09-30 17:33:08 +0800

    fbgdbd

--- Facility Reservation Kiosk/IPadKioskWebService/GetFacilityReservations.aspx.cs
+++ Facility Reservation Kiosk/IPadKioskWebService/GetFacilityReservations.aspx.cs
@@ -11,7 +11,26 @@ namespace IPadKioskWebService
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //To get the string to search in facility table
+            string departmentID = Request.QueryString["DepartmentID"];
+            string block = Request.QueryString["Block"];
+            string level = Request.QueryString["Level"];
+            string name = Request.QueryString["Name"];
+            //format of date yyyy-MM-dd
+            string date = Request.QueryString["Date"];
 
+            //if date is blank, return current system date
+            if( date == null )
+            {
+                int dateCurrentYear = DateTime.Now.Year;
+                int dateCurrentMonth = DateTime.Now.Month;
+                int dateCurrentDay = DateTime.Now.Day;
+            }
+            //else return only specified date
+            else
+            {
+
+            }
         }
     }
 }
