a1401eb838899466606e01e696e5c09086528303
celiatan <131178t@mymail.nyp.edu.sg>
2015-10-26 15:57:56 +0800

    sas

--- Facility Reservation Kiosk/Facility Reservation Kiosk/RevokePage.aspx.cs
+++ Facility Reservation Kiosk/Facility Reservation Kiosk/RevokePage.aspx.cs
@@ -11,38 +11,48 @@ namespace Facility_Reservation_Kiosk
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            lblDeviceID.Text = Request.QueryString["searchID"];
 
-            int ID = System.Convert.ToInt32(lblDeviceID.Text);
-
-            using (var db = new FacilityReservationKioskEntities())
             {
-                //Basic select query from a single table 
-                var device = db.Devices.Find(ID);
+                lblDeviceID.Text = Request.QueryString["searchID"];
 
-                //Loop through to print out
-                lblDescription.Text = device.Description;
+                int ID = System.Convert.ToInt32(lblDeviceID.Text);
+
+                using (var db = new FacilityReservationKioskEntities())
+                {
+                    //Basic select query from a single table 
+                    var device = db.Devices.Find(ID);
+
+                    //Loop through to print out
+                    lblDescription.Text = device.Description;
+                }
             }
         }
 
         protected void btnConfirm_Click(object sender, EventArgs e)
         {
-
-            int ID = System.Convert.ToInt32(lblDeviceID.Text);
-
-            using (var db = new FacilityReservationKioskEntities())
+            if (tbReason.Text == "")
             {
-                Device device = db.Devices.Find(ID);
-
-                //Modify fields
-                device.Status = "REV";
-                device.RejectedOrRevokedDateTime = DateTime.Now;
-                device.RejectedOrRevokedReason = tbReason.Text;
-
-                db.SaveChanges();
+                lblMsg.Text = "Please enter revoke reason!";
             }
 
-            lblMsg.Text = "Device revoked successfully";
+            else
+            {
+                int ID = System.Convert.ToInt32(lblDeviceID.Text);
+
+                using (var db = new FacilityReservationKioskEntities())
+                {
+                    Device device = db.Devices.Find(ID);
+
+                    //Modify fields
+                    device.Status = "REV";
+                    device.RejectedOrRevokedDateTime = DateTime.Now;
+                    device.RejectedOrRevokedReason = tbReason.Text;
+
+                    db.SaveChanges();
+                }
+
+                lblMsg.Text = "Device revoked successfully";
+            }
         }
     }
 }
