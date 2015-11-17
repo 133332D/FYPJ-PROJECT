f0a19b2df9abba877a350cacf0f3ad07f55eea2a
limyulin <lim.yulin@hotmail.com>
2015-11-17 16:44:58 +0800

    added new

---
+++ Facility Reservation Kiosk/IPadKioskWebService/UpdateCameraDensity.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IPadKioskWebService
+{
+    public partial class UpdateCameraDensity : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string cameraID = Request["cameraID"];
+                string density = Request["density"];
+
+                 using (var db = new FacilityReservationKioskEntities())
+                 {
+
+                    var cam =  db.Cameras.Find(Convert.ToInt32(cameraID));
+                     if(cam != null)
+                     {
+                         cam.CurrentDensity = Convert.ToDouble(density);
+                         db.SaveChanges();
+                     }
+                 }
+            }
+        }
+    }
+}
